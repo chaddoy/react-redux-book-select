012e091956da24014ef9cd5764daf962f0ec96c2
Chad Dumadag <capdumadag@gmail.com>
2017-02-01 11:10:14 +0800

    initial commit

---
+++ src/containers/book-list.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions';
+import { bindActionCreators } from 'redux';
+
+class BookList extends React.Component {
+
+	renderList() {
+		return this.props.books.map( (book, index ) => {
+			return (
+				<li className="list-group-item"
+					key={ index }
+					onClick={ () => this.props.selectBook(book) }>
+					{ book.title }
+				</li>
+			);
+		} );
+	}
+
+	render() {
+		return (
+			<ul className="list-group col-sm-4">
+				{ this.renderList() }
+			</ul>
+		);
+	}
+
+}
+
+function mapStateToProps( state ) {
+	// Whatever is returned will show up as props
+	// inside of BookList
+	return {
+		books: state.books
+	};
+}
+
+
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps ( dispatch ) {
+	// Whenever selectBook is called, the result should be passed
+	// to all of our reducers
+	return bindActionCreators( { selectBook: selectBook }, dispatch );
+}
+
+
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make it available
+// as prop.
+export default connect( mapStateToProps, mapDispatchToProps )( BookList );
---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{ title: 'Ready Player One', pages: 69 },
+		{ title: 'The Trials of Apollo', pages: 180 },
+		{ title: 'Modern Romance', pages: 546 },
+		{ title: 'Javascript: The Good Parts', pages: 113 }
+	];
+}
